0b2f8fedfb29fbf966fae9d3788ffd9fb9c3f7e7
abhishekhotti <abhishek.hotti@sjsu.edu>
2019-03-03 18:51:53 -0800

    added first line back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,11 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-            and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
             <br />
             Hello World
           </p>
