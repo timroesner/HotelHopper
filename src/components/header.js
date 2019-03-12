08ca0814fbdc3e0e7a23e0f2c57d1aeb94c676de
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-12 00:36:06 -0700

    Move tailwind into real dependencies

--- src/components/header.js
+++ src/components/header.js
@@ -13,18 +13,18 @@ class Header extends React.Component {
    navigateTo(page) {
       this.props.history.push(`/${page}`);
    }
-   
+
 	render() {
     return (
        <div className="h-16 w-full bg-white border-b-2 flex items-center fixed">
-         <img src={logo} className="ml-8 h-4/5" alt="logo" />
-         <p className="ml-3 text-soft-blue font-sans text-xl font-bold tracking-wide leading-none cursor-pointer" onClick={() => this.navigateTo("")}>Hotel<br/>Hopper</p>
+         <img src={logo} className="ml-8 h-4/5 cursor-pointer" alt="logo" onClick={() => this.navigateTo("")} />
+         <p className="ml-3 text-soft-blue font-sans text-xl font-bold leading-none cursor-pointer" onClick={() => this.navigateTo("")}>Hotel<br/>Hopper</p>
          {
             this.props.user ? 
             <div></div> :
             <div className="mr-8 ml-auto">
-               <Button className="mr-4 bg-soft-blue rounded text-white p-2 font-sans text-xl font-bold tracking-wide" onClick={() => this.navigateTo("signup")}>Sign up</Button>
-               <Button className="font-sans text-xl font-bold tracking-wide" onClick={() => this.navigateTo("login")}>Log in</Button>
+               <Button className="mr-4 bg-soft-blue rounded text-white p-2 font-sans text-xl font-bold" onClick={() => this.navigateTo("signup")}>Sign up</Button>
+               <Button className="font-sans text-xl font-bold" onClick={() => this.navigateTo("login")}>Log in</Button>
             </div>
          }
        </div>
