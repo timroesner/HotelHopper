d48def68ddc9eabf3534d5d64c936dbae43f020d
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-26 15:39:43 -0700

    Format date for search parameters

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -95,7 +95,7 @@ class LandingPage extends Component {
  search() {
    const location = this.state.location
    if(location && this.state.startDate.format("L") && this.state.endDate.format("L") && this.state.people && this.state.rooms) {
-    this.props.history.push(`/search?lat=${location.lat}&long=${location.lng}&startDate=${this.state.startDate}&endDate=${this.state.endDate}&people=${this.state.people}&rooms=${this.state.rooms}`);
+    this.props.history.push(`/search?lat=${location.lat}&long=${location.lng}&startDate=${this.state.startDate.format("L")}&endDate=${this.state.endDate.format("L")}&people=${this.state.people}&rooms=${this.state.rooms}`);
    } else {
      alert("Please fill all fields")
    }
