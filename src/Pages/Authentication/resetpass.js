8332e9902f3e43cb0f86784598cf105a5182eb2d
Hung Tang <hung.tang@sjsu.edu>
2019-03-26 20:59:57 -0700

    Added forgotpassword and reset password

--- src/Pages/Authentication/resetpass.js
+++ src/Pages/Authentication/resetpass.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
+import Resetcomp from '../../components/resetpasscomponent';
 
 class Reset extends Component {
+  
   render() {
     return (
       <div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hotel Hopper's Temporary Reset Password Page
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Resetcomp />
       </div>
     );
   }
---
+++ src/helper/endpoints.js
@@ -0,0 +1,5 @@
+const api = process.env.NOW_GITHUB_COMMIT_REF === 'master' ? 
+"https://api.hotelhopper.cf" : 
+"https://staging-api.hotelhopper.cf"
+
+export default api;
