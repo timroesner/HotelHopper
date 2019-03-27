425e15e0e5573638b60beed51da1e2b585f7a324
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-27 11:27:28 -0700

    Update signup name passing

--- src/Pages/Authentication/signup.js
+++ src/Pages/Authentication/signup.js
@@ -68,7 +68,7 @@ class Signup extends React.Component {
                         let tokenKey = "token";
                         let tokenValue = data["data"]["token"];
                         window.localStorage.setItem(tokenKey, tokenValue);
-                        window.localStorage.setItem("name", this.state.firstname);
+                        window.localStorage.setItem("currentUser", JSON.stringify({first_name: this.user.first_name}));
                     }
                     if (success) {
                         this.props.history.push('/');
