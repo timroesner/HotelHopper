24e4e7fa327c999b59a465874c983922dbb5ea0e
Zhe <dennislee0705@gmail.com>
2019-04-15 09:39:45 -0700

    add BookingDetailComp

--- src/Pages/Reservation Related/confirmation.js
+++ src/Pages/Reservation Related/confirmation.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.svg';
+import BookingDetailComp from '../../components/bookingDetailComp';
 
 class Confirmation extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    let booking = {
+      hotel: {
+          title: "MGM Grand Hotel & Casino",
+          image: "https://mgmgrand.mgmresorts.com/content/dam/MGM/mgm-grand/hotel/mgm-grand/exterior/mgm-grand-hotel-mgm-grand-exterior-hero-shot-@2x.jpg",
+          address: "3799 Las Vegas Blvd S, Las Vegas, NV",
+          rating: "4.1/5 Very Good"
+      },
+      id: "123",
+      roomType: "Grand Queen Room",
+      startDate: "2019-06-08",
+      endDate: "2019-06-14",
+      status: 'approved'
+    }
+
+    this.state = {
+      booking: booking
+    }
+  }
+
   render() {
     console.log(this.props.match.params.params);
     if(this.props.match.params.params === undefined){
       return (
-      <div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hotel Hopper's Temporary Post-Checkout Confirmation Page
+        <div>
+        
+          
+          <p className="md:text-4xl lg:text-3xl text-lg text-dark-blue font-sans font-bold mb-4 md:mb-12">
+              Your Booking Details
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          
+          <BookingDetailComp reservation = {this.state.booking}/>
+
+        
+        </div>
       </div>
     );
   }
---
+++ src/components/bookingDetailComp.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import * as moment from 'moment';
+import StarRating from 'react-star-ratings';
+
+class BookingDetailComp extends React.Component {
+
+  render() {
+    const reservation = this.props.reservation
+    return(
+      <div key={reservation.id} className="w-full h-20 md:h-64 flex items-flex mt-8 justify-center">
+        <img alt="Hotel" src={reservation.hotel.image} className="h-full w-auto rounded"/>
+        
+        <div className="ml-4 md:ml-8 w-auto">
+                    <p className="font-bold md:text-2xl">{reservation.hotel.title}</p>
+                    <div className="">
+                    <StarRating name="rating" size={10} totalStars={5} rating={4}/>
+                    </div>
+                    
+                    <p className="mt-1 md:mt-2 text-xs md:text-lg">{reservation.hotel.address}</p>
+                    <p className="mt-1 md:mt-2 text-xs md:text-lg">{reservation.hotel.rating}</p>
+
+                    <p className="mt-3 md:mt-8 text-sm md:text-xl">{"Room: " + reservation.roomType}</p>
+                    <p className="mt-1 md:mt-4 text-sm md:text-xl">{"Check-in: " + moment(reservation.startDate).format("MMM D, YYYY")}</p>
+                    <p className="mt-1 md:mt-4 text-sm md:text-xl">{"Check-out: " + moment(reservation.endDate).format("MMM D, YYYY")}</p>
+                </div>
+
+      </div>
+      
+      
+
+    
+    )
+  }
+
+}
+
+export default withRouter(BookingDetailComp);
