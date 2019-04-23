c2193f1f47a65e3175790ae8b2a498573df31c4b
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-23 15:17:54 -0700

    Add fetch functions

--- src/Pages/Reservation Related/checkout.js
+++ src/Pages/Reservation Related/checkout.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
+import api from '../../helper/endpoints';
 
 class Checkout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: '',
+      user: {},
+      reservation: {
+        startDate: "2019-08-05",
+        endDate: "2019-08-08",
+        hotelId: "23",
+        rooms: {
+          "Single": 1,
+          "Double": 1
+        }
+      },
       error: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.loadUserData()
+    this.parseParams()
+    this.loadHotelInfo()
+  }
+
+  loadUserData = () => {
+    const token = window.localStorage.getItem("token")
+    if(token !== null) {
+      fetch(api + "/auth/userDetails", {
+        method: "GET",
+        headers: {
+          'accept': 'application/json',
+          'Authorization': "Bearer "+token
+        }
+      }).then(results => {
+          return results.json();
+      }).then(data => {
+        this.setState({user: data['data']})
+      })
+    } else {
+      this.props.history.push(`/login`);
+    }
+  }
+
+  parseParams = () => {
+    
+  }
+
+  loadHotelInfo = () => {
+    const reservation = this.state.reservation
+    fetch(api + `/hotels/${reservation.hotelId}?startDate=${reservation.startDate}&endDate=${reservation.endDate}`, {
+      method: "GET",
+      headers: {
+        'accept': 'application/json'
+      }
+    }).then(results => {
+        return results.json();
+    }).then(data => {
+      console.log(data)
+      this.setState({hotel: data['data']})
+    })
   }
 
   handleChange(event) {
     this.setState({
-      [event.target.id]: event.target.value
+      user: {[event.target.id]: event.target.value}
     });
   }
 
@@ -55,14 +102,14 @@ class Checkout extends Component {
               <p className="text-sans font-bold text-2xl">Basic Information</p>
               <div className="mt-4 flex items-flex">
                 <input className="shadow appearance-none bg-white font-bold mr-4 border border-soft-blue w-full rounded h-14  py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-                id="firstName" value={this.state.firstName} onChange={this.handleChange} type="text" placeholder="First Name" />
+                id="firstName" value={this.state.user.firstName} onChange={this.handleChange} type="text" placeholder="First Name" />
                 <input className="shadow appearance-none bg-white font-bold mr-4 border border-soft-blue w-full rounded h-14  py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-                id="lastName" value={this.state.lastName} onChange={this.handleChange} type="text" placeholder="Last Name" />
+                id="lastName" value={this.state.user.lastName} onChange={this.handleChange} type="text" placeholder="Last Name" />
               </div>
               <input className="shadow appearance-none bg-white font-bold border border-soft-blue w-3/5 rounded h-14 mt-4 py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-                id="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="E-mail" />
+                id="email" value={this.state.user.email} onChange={this.handleChange} type="text" placeholder="E-mail" />
               <input className="shadow appearance-none bg-white font-bold border border-soft-blue w-3/5 rounded h-14 mt-4 py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-                id="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} type="text" placeholder="Phone Number" />
+                id="phoneNumber" value={this.state.user.phoneNumber} onChange={this.handleChange} type="text" placeholder="Phone Number" />
             </div>
             {this.rewardsDiv()}
           </div>
