c9e0642a59b5d78c2a7d0d0625aab6a7097a4807
abhishekhotti <abhishek.hotti@sjsu.edu>
2019-04-22 18:58:01 -0700

    checking

--- src/Pages/Profile/rewards.js
+++ src/Pages/Profile/rewards.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import SideMenu from '../../components/sidemenu';
+import React, { Component } from "react";
+import SideMenu from "../../components/sidemenu";
 
 class Rewards extends Component {
   render() {
     return (
       <div className="mt-4 md:mt-16 flex items-flex pb-8">
-        {
-          window.innerWidth > 415 &&
-          <SideMenu selected="Rewards" items={["Profile", "Billing Info", "Trips", "Rewards"]} />
-        }
-        <div className="ml-8 md:ml-24 ">
-          Your content here
-        </div>
+        {window.innerWidth > 415 && (
+          <SideMenu
+            selected="Rewards"
+            items={["Profile", "Billing Info", "Trips", "Rewards"]}
+          />
+        )}
+        <div className="ml-8 md:ml-24 ">Your content here lahsdfkjhs</div>
       </div>
     );
   }
