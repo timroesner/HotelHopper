af17d68b29341bd48a9a9800accc578fbee149ed
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-25 21:36:02 -0700

    Make rewards card responsive

--- src/components/rewardsCard.js
+++ src/components/rewardsCard.js
@@ -1,34 +1,26 @@
 import React, { Component } from "react";
-import logo from "../assets/logo.svg";
 
 class RewardsCard extends Component {
   render() {
-    const points = this.props.points;
     return (
-      <div className="container mx-auto">
-        <div className="flex mt-4 justify-center">
-          <div className="bg-soft-blue rounded-lg max-w-sm md:max-w-lg text-white font-sans font-bold md:h-64">
-            <div className="flex items-flex justify-center">
-              <div className="md:h-64">
-                <div className="mt-2 md:mt-6 ml-2 md:ml-8">
-                  <p className="text-xs md:text-2xl">Hotel Hopper</p>
-                  <p className="text-xs md:text-3xl">Rewards Program</p>
-                </div>
-                <p className="text-xs md:text-3xl mt-2 md:mt-6 ml-2 md:ml-8 opacity-75">
-                  This is the rewards stuff and you have these many points
-                  available
-                </p>
-              </div>
-              <div className="mt-2 md:mt-6 md:pl-6 md:h-64">
-                <p className="text-xs md:text-2xl text-soft-blue"> ,</p>
-                <p className="text-xs text-center md:text-3xl">
-                  Reward Points:
-                </p>
-                <p className="text-4xl md:text-8xl text-center pl-4 pr-4 md:pl-12 md:pr-12 tracking-tight ">
-                  {points}
-                </p>
-              </div>
+      <div className="bg-soft-blue rounded-lg md:rounded-xl md:max-w-xl mx-auto text-white font-sans font-bold h-28 md:h-64">
+        <div className="flex items-flex justify-center">
+          <div className="w-3/5 md:h-64 ml-2 md:ml-4">
+            <div className="mt-2 md:mt-6">
+              <p className="text-sm md:text-2xl">Hotel Hopper</p>
+              <p className="text-base md:text-3xl">Rewards Program</p>
             </div>
+            <p className="text-xs md:text-2xl mt-4 md:mt-8 opacity-75">
+              These points can be redeemed for a free stay. You earn them by staying with us.
+            </p>
+          </div>
+          <div className="mt-4 md:mt-8 md:pl-6 md:h-64">
+            <p className="text-sm text-center md:text-3xl">
+              Reward Points:
+            </p>
+            <p className="text-5xl md:text-8xl text-center pl-4 pr-4 mt-2 md:pl-12 md:pr-12 tracking-tight ">
+              {this.props.points}
+            </p>
           </div>
         </div>
       </div>
