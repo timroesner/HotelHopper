4ba40cd44b33d43d113be3a99bd8b97acf25ec4f
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-25 21:36:57 -0700

    Make rewards page use rewardsComponent

--- src/Pages/Profile/rewards.js
+++ src/Pages/Profile/rewards.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
 import SideMenu from '../../components/sidemenu';
+import RewardsCard from  '../../components/rewardsCard';
+import api from '../../helper/endpoints';
 
 class Rewards extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentWillMount() {
+    this.fetchPoints()
+  }
+
+  fetchPoints() {
+    const token = window.localStorage.getItem("token")
+    if(token !== null) {
+      fetch(api + "/auth/userDetails", {
+        method: "GET",
+        headers: {
+          'accept': 'application/json',
+          'Authorization': "Bearer "+token
+        }
+      }).then(results => {
+          return results.json();
+      }).then(data => {
+        this.setState({user: data['data']})
+      })
+    } else {
+      this.props.history.push(`/login`);
+    }
+  }
+
   render() {
-    const points = this.props.points;
     return (
       <div className="mt-4 md:mt-16 flex items-flex pb-8">
         {
           window.innerWidth > 415 &&
           <SideMenu selected="Rewards" items={["Profile", "Billing Info", "Trips", "Rewards"]} />
         }
-        <div className="ml-8 md:ml-24 ">
-          <div className="container mx-auto">
-        <div className="flex mt-4 justify-center">
-          <div className="bg-soft-blue rounded-lg max-w-sm md:max-w-lg text-white font-sans font-bold md:h-64">
-            <div className="flex items-flex justify-center">
-              <div className="md:h-64">
-                <div className="mt-2 md:mt-6 ml-2 md:ml-8">
-                  <p className="text-xs md:text-2xl">Hotel Hopper</p>
-                  <p className="text-xs md:text-3xl">Rewards Program</p>
-                </div>
-                <p className="text-xs md:text-3xl mt-2 md:mt-6 ml-2 md:ml-8 opacity-75">
-                  This is the rewards stuff and you have these many points
-                  available
-                </p>
-              </div>
-              <div className="mt-2 md:mt-6 md:pl-6 md:h-64">
-                <p className="text-xs md:text-2xl text-soft-blue"> ,</p>
-                <p className="text-xs text-center md:text-3xl">
-                  Reward Points:
-                </p>
-                <p className="text-4xl md:text-8xl text-center pl-4 pr-4 md:pl-12 md:pr-12 tracking-tight ">
-                  {points}
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="w-full-w/o-margins max-w-lg mx-auto">
+          <RewardsCard points={this.state.user.rewardPoints}/>
         </div>
       </div>
     );
