9d187b52b9586ceb7ecc1038e495869cf4cca5e6
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-26 18:45:31 -0700

    Adhere to Design and make rewards page responsive

--- src/Pages/Profile/rewards.js
+++ src/Pages/Profile/rewards.js
@@ -5,32 +5,30 @@ import api from "../../helper/endpoints";
 
 class Rewards extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       user: {}
-    };
+    }
   }
 
   componentWillMount() {
-    this.fetchPoints();
+    this.fetchPoints()
   }
 
   fetchPoints() {
-    const token = window.localStorage.getItem("token");
-    if (token !== null) {
+    const token = window.localStorage.getItem("token")
+    if(token !== null) {
       fetch(api + "/auth/userDetails", {
         method: "GET",
         headers: {
-          accept: "application/json",
-          Authorization: "Bearer " + token
+          'accept': 'application/json',
+          'Authorization': "Bearer "+token
         }
-      })
-        .then(results => {
+      }).then(results => {
           return results.json();
-        })
-        .then(data => {
-          this.setState({ user: data["data"] });
-        });
+      }).then(data => {
+        this.setState({user: data['data']})
+      })
     } else {
       this.props.history.push(`/login`);
     }
@@ -38,37 +36,25 @@ class Rewards extends Component {
 
   render() {
     return (
-      <div>
-        <div className="mt-4 md:mt-16 flex items-flex pb-8">
-          {window.innerWidth > 415 && (
-            <SideMenu
-              selected="Rewards"
-              items={["Profile", "Billing Info", "Trips", "Rewards"]}
-            />
-          )}
-          <div className="w-full-w/o-margins mx-auto md:px-24">
-            <RewardsCard points={this.state.user.rewardPoints} />
-            <hr className="mt-4 border" />
-            <h1 className="text-black mt-4 mb-4">Frequently asked questions</h1>
-            <div className="text-grey">
-              <h2>Q: Why do you need my credit card details?</h2>
-              <h3 className="text-grey-light pb-2">
-                A: They are required as a gaurantee in case you are a no show or
-                cancel too late.
-              </h3>
-              <h2>Q: How do I earn points?</h2>
-              <h3 className="text-grey-light pb-2">
-                A: You earn 1 reward point per $10 dollars spent. Reward points
-                will be deposited to your account after competion of your stay.
-              </h3>
-              <h2>Q: When can I use reward points?</h2>
-              <h3 className="text-grey-light">
-                A: You can use reward points towards your stay if you your
-                reward points can cover the total cost of your reservation (1
-                point = $2 dollars).
-              </h3>
-            </div>
-          </div>
+      <div className="mt-4 md:mt-16 flex items-flex pb-8">
+        {
+          window.innerWidth > 415 &&
+          <SideMenu selected="Rewards" items={["Profile", "Billing Info", "Trips", "Rewards"]} />
+        }
+        <div className="w-full-w/o-margins mx-auto md:px-24">
+          <RewardsCard points={this.state.user.rewardPoints}/>
+          <hr className="mt-4 border-b"/>
+          <h1 className="text-2xl md:text-4xl text-black my-4">Frequently asked questions</h1>
+          <p className="md:text-2xl font-semibold">How do I earn points?</p>
+          <p className="md:text-xl text-grey-dark mt-2 mb-8">
+            You earn 1 reward point per $10 dollars spent. Reward points
+            will be deposited to your account after you completed your stay.
+          </p>
+          <p className="md:text-2xl font-semibold">When can I use reward points?</p>
+          <p className="md:text-xl text-grey-dark mt-2 mb-8">
+            You can redeem your reward points for a free stay at one of our hotels. The option will automatically appear during checkout
+            once you have accumalted enough points. 2 points will be equivilent to $1.
+          </p>
         </div>
       </div>
     );
