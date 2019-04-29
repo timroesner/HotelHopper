135dfad42d4544a099e160614e34432d536ce84e
Zhe <dennislee0705@gmail.com>
2019-04-29 16:38:26 -0700

    bug fix

--- src/components/bookingDetailComp.js
+++ src/components/bookingDetailComp.js
@@ -70,7 +70,7 @@ class BookingDetailComp extends React.Component {
             <p className="mt-4 text-sm md:text-lg">
               {"Total paid: "}
               <span className="float-right">
-                {"$"+(reservation.totalPrice*0.09).toFixed(2)}
+                {"$"+(reservation.totalCost*0.09).toFixed(2)}
               </span>
             </p>
             <p className="mt-4 text-sm md:text-lg">
