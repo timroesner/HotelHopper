8e3510b78d4c0e1d70c5cec273e629ee65bea832
abhishekhotti <abhishek.hotti@sjsu.edu>
2019-04-29 17:49:44 -0700

    fixing sizing

--- src/components/bookingDetailComp.js
+++ src/components/bookingDetailComp.js
@@ -4,16 +4,15 @@ import * as moment from "moment";
 import StarRating from "react-star-ratings";
 
 class BookingDetailComp extends React.Component {
-
   createRoomsString = () => {
-    var string = ""
-    if(this.props.reservation.reservedRooms !== undefined) {
+    var string = "";
+    if (this.props.reservation.reservedRooms !== undefined) {
       this.props.reservation.reservedRooms.forEach(room => {
-        string += "\u00a0\u00a0\u00a0"+room.count+" "+room.roomType.title
-      })
+        string += "\u00a0\u00a0\u00a0" + room.count + " " + room.roomType.title;
+      });
     }
-    return string
-  }
+    return string;
+  };
 
   render() {
     const reservation = this.props.reservation;
@@ -21,39 +20,39 @@ class BookingDetailComp extends React.Component {
       <div>
         <div
           key={reservation.id}
-          className="w-full flex flex-col md:flex-row items-flex"
+          className="w-full flex flex-col md:flex-row items-stretch "
         >
           <img
             alt="Hotel"
             src={reservation.hotel.imageUrl}
-            className="h-48 md:h-64 w-auto rounded md:rounded-lg"
+            className="h-auto w-auto rounded md:rounded-lg"
           />
-          <div className="mt-4 md:mt-0 md:ml-12 w-auto">
+          <div className="mt-4 md:mt-0 md:ml-12 w-1/2">
             <p className="font-bold md:text-2xl">{reservation.hotel.title}</p>
             <div className="mt-2">
-            <StarRating
+              <StarRating
                 rating={reservation.hotel.stars}
                 numberOfStars={reservation.hotel.stars}
                 starRatedColor="#597aee"
                 starSpacing="3px"
                 starDimension="20px"
-                name='rating'
-            />
+                name="rating"
+              />
             </div>
 
             <p className="mt-2 text-sm md:text-lg">
-              {`${reservation.hotel.street}, ${reservation.hotel.city}, ${reservation.hotel.state}`}
+              {`${reservation.hotel.street}, ${reservation.hotel.city}, ${
+                reservation.hotel.state
+              }`}
             </p>
 
             <p className="mt-2 md:text-lg font-medium">
-              {reservation.hotel.rating+" / 10 User rating"}
+              {reservation.hotel.rating + " / 10 User rating"}
             </p>
 
             <p className="mt-8 text-sm md:text-lg">
               {"Rooms: "}
-              <span className="float-right">
-                {this.createRoomsString()}
-              </span>
+              <span className="float-right">{this.createRoomsString()}</span>
             </p>
             <p className="mt-4 text-sm md:text-lg">
               {"Check-in: "}
@@ -70,14 +69,12 @@ class BookingDetailComp extends React.Component {
             <p className="mt-4 text-sm md:text-lg">
               {"Total paid: "}
               <span className="float-right">
-                {"$"+(reservation.totalCost*1.09).toFixed(2)}
+                {"$" + (reservation.totalCost * 1.09).toFixed(2)}
               </span>
             </p>
             <p className="mt-4 text-sm md:text-lg">
               {"Cancellation fee: "}
-              <span className="float-right">
-                {"$93"}
-              </span>
+              <span className="float-right">{"$93"}</span>
             </p>
           </div>
         </div>
